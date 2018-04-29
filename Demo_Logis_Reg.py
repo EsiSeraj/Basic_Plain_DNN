e8db6ecce6aae6ed9729baca1928e914f45e4a62
Esi <esmaeil.seraj09@gmail.com>
2018-04-29 19:35:32 -0400

    New Revisions

--- Demo_Logis_Reg.py
+++ Demo_Logis_Reg.py
@@ -3,8 +3,25 @@
 Created on Sun Apr 29 13:21:50 2018
 
 @author: Esmaeil Seraj <esmaeil.seraj09@gmail.com>
+@website: https://github.com/EsiSeraj/
 
-Demo Script for Codes in This Repository
+Demo Script for "Logis_Reg.py", a logistic regression model generator with all
+required helper functions
+
+Copyright (C) <2018>  <Esmaeil Seraj>
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import numpy as np
@@ -13,13 +30,10 @@ import numpy as np
 # # "Logis_Reg.py" test: Logistic Regression
 # This module includes all of the helper functions for logistic regression
 # included functions: "sigmoid()", "initialize_params()", propagate(), 
-# optimize(), 
+# optimize(), predict()
 # =============================================================================
 
 import Logis_Reg as LR
-# "from Logis_Reg import sigmoid" this is another way of doing it, but it just
-# imports the function that you just called. This way writting simply "sigmoid"
-# is the command to use the sigmoid function instead of "LR.sigmoid()".
 
 ## sigmoid function
 activation_val = LR.sigmoid(np.array([0, 2]))
@@ -34,7 +48,7 @@ print("in case of " + str(case1) + " initialization, w=" + str(w1))
 print("in case of " + str(case2) + " initialization, w=" + str(w2))
 
 ## forward and backward propagation
-X, Y = np.array([[1.,2.,-1.],[3.,4.,-3.2]]), np.array([[1,0,1]])
+X, Y = np.array([[1., 2., -1.], [3., 4., -3.2]]), np.array([[1, 0, 1]])
 grads, cost = LR.propagate(w2, b2, X, Y)
 print ("dw = " + str(grads["dw"]))
 print ("db = " + str(grads["db"]))
@@ -48,5 +62,18 @@ print ("b = " + str(params["b"]))
 print ("dw = " + str(grads["dw"]))
 print ("db = " + str(grads["db"]))
 
-## 
+## prediction
+labels = LR.predict(w2, b2, X)
+print("predicted labels are y_hat=" + str(labels))
 
+## logistic regression model
+np.random.seed(1)
+X_train = np.random.randn(100, 500)
+Y_train = np.random.randint(2, size=(1, X_train.shape[1]))
+X_test = np.random.randn(100, 200)
+Y_test = np.random.randint(2, size=(1, X_test.shape[1]))
+lr_mdl = LR.logis_reg_model(X_train, Y_train, X_test, Y_test, init_case = "zero", 
+                    num_iter = 2000, learning_rate = 0.005, print_cost = True)
+
+###############################################################################
+###############################################################################
--- Logis_Reg.py
+++ Logis_Reg.py
@@ -3,17 +3,11 @@
 Created on Sun Apr 29 12:20:22 2018
 
 @author: Esmaeil Seraj <esmaeil.seraj09@gmail.com>
-
-# The main steps for building a Neural Network are:
-# 1. Define the model structure (such as number of input features) 
-# 2. Initialize the model's parameters
-# 3. Loop:
-#     - Calculate current loss (forward propagation)
-#     - Calculate current gradient (backward propagation)
-#     - Update parameters (gradient descent)
+@website: https://github.com/EsiSeraj/
 
 Logistic Regression
-    - All Required Helper functions
+    - All required helper functions
+    - A comprehensive logistic regression model generator
 
 Reguired Packages
     - numpy
@@ -21,6 +15,33 @@ Reguired Packages
 # NOTE: this function gets regular updats; for now, it only includes equations
     and computations for sigmoid non-linearity, additional non-linearities are
     to be added in the future
+    
+Copyright (C) <2018>  <Esmaeil Seraj>
+    
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+"""
+# The main steps for building a Neural Network are:
+     1. Define the model structure (such as number of input features) 
+     2. Initialize the model's parameters
+     3. Loop:
+         - Calculate current loss (forward propagation)
+         - Calculate current gradient (backward propagation)
+         - Update parameters (gradient descent)
+     4. Use learnt parameters to predict the labels on train data
+     5. Use learnt parameters to predict the labels on test data
 """
 
 # In[0]: loading packages
@@ -59,7 +80,6 @@ def initialize_params(dim, case = "zero" ):
     b -- initialized bias scalar
     
     NOTE: unlike DNN, for logistic regression it is OK to initialize with zeros
-    
     """
     
     if case == "zero":
@@ -92,7 +112,6 @@ def propagate(w, b, X, Y):
     cost -- negative log-likelihood cost for logistic regression
     dw -- gradient of the loss with respect to w, thus same shape as w
     db -- gradient of the loss with respect to b, thus same shape as b
-    
     """
     
     m = X.shape[1]
@@ -115,9 +134,9 @@ def propagate(w, b, X, Y):
     
     return grads, cost
 
-# In[4]: Optimization
+# In[4]: optimization (gradient descent)
 
-def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = True):
+def optimize(w, b, X, Y, num_iter, learning_rate, print_cost = True):
     """
     This function optimizes w and b by running a gradient descent algorithm
     
@@ -126,21 +145,21 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = True):
     b -- bias, a scalar
     X -- data of shape (dim, number of examples)
     Y -- true "label" vector (0/1 binary), of shape (1, number of examples)
-    num_iterations -- number of iterations of the optimization loop
+    num_iter -- number of iterations of the optimization loop
     learning_rate -- learning rate of the gradient descent update rule
     print_cost -- True to print the loss every 100 steps
     
     Returns:
     params -- dictionary containing the weights w and bias b
-    grads -- dictionary containing the gradients of the weights and bias with respect to the cost function
-    costs -- list of all the costs computed during the optimization, this will be used to plot the learning curve.
-    
+    grads -- dictionary containing the gradients of the weights and bias with 
+            respect to the cost function
+    costs -- list of all the costs computed during the optimization, this will 
+            be used to plot the learning curve.
     """
     
     costs = []
     
-    for i in range(num_iterations):
-        
+    for i in range(num_iter):     
         # compute gradiantes
         grads, cost = propagate(w, b, X, Y)
         dw = grads["dw"]
@@ -166,7 +185,93 @@ def optimize(w, b, X, Y, num_iterations, learning_rate, print_cost = True):
     
     return params, grads, costs
 
-# In[5]: 
+# In[5]: prediction
 
+def predict(w, b, X):
+    '''
+    This function Predicts whether the label is 0 or 1 using learned logistic 
+    regression parameters (w, b)
+    
+    Arguments:
+    w -- weights, a numpy array of size (dim, 1)
+    b -- bias, a scalar
+    X -- data of size (dim, number of examples)
+    
+    Returns:
+    Y_prediction -- a numpy array (vector) containing all predictions (0/1) for
+    the examples in X
+    
+    NOTE: the threshold for output probabilities is <th=0.5> where any 
+    prediction with a greater value will be labled as '1' and vice versa
+    '''
+    
+    m = X.shape[1]
+    Y_prediction = np.zeros((1, m))
+    w = w.reshape(X.shape[0], 1)
+    
+    A = sigmoid(np.dot(w.T, X) + b) # compute probabilities
+    
+    for i in range(A.shape[1]):     
+        # Convert probabilities A[0,i] to actual predictions p[0,i]
+        zero_index = np.where(A >= .5, A, 0)
+        one_index = np.where(zero_index < .5, zero_index, 1)
+        Y_prediction = one_index
+    
+    assert(Y_prediction.shape == (1, m))
+    
+    return Y_prediction
 
+# In[6]: logistic regression model
+    
+def logis_reg_model(X_train, Y_train, X_test, Y_test, init_case = "zero", 
+                    num_iter = 2000, learning_rate = 0.5, print_cost = True):
+    """
+    This function Builds a logistic regression model based upon your train data
+    
+    Arguments:
+    X_train -- training set represented by an array of shape (dim, m_train)
+    Y_train -- training labels represented by an array of shape (1, m_train)
+    X_test -- test set represented by an array of shape (dim, m_test)
+    Y_test -- test labels represented by an array of shape (1, m_test)
+    num_iter -- hyperparameter: number of iterations for optimization, 
+                        (default = 2000)
+    learning_rate -- hyperparameter: used in updating stage, (default = 0.5)
+    print_cost -- Set to true to print the cost every 100 iterations, (default 
+                   = True)
+    
+    Returns:
+    lr_mdl -- dictionary containing information about the model
+    
+    NOTE: in addition to the model information, this function also prints the
+    accuracies for train and test data
+    """
+    
+    # initialize parameters with zeros
+    w, b = initialize_params(X_train.shape[0], init_case)
+
+    # Gradient descent
+    parameters, grads, costs = optimize(w, b, X_train, Y_train, num_iter, learning_rate, print_cost)
+    w = parameters["w"]
+    b = parameters["b"]
+    
+    # Predict test/train set examples
+    Y_prediction_test = predict(w, b, X_test)
+    Y_prediction_train = predict(w, b, X_train)
+
+    # Print train/test Errors
+    print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train))*100))
+    print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test))*100))
+
+    
+    lr_mdl = {"costs": costs,
+         "Y_prediction_test": Y_prediction_test, 
+         "Y_prediction_train" : Y_prediction_train, 
+         "w" : w, 
+         "b" : b,
+         "learning_rate" : learning_rate,
+         "num_iter": num_iter}
+    
+    return lr_mdl
+
+# In[]:
 
